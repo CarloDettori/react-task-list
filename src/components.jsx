7def7bc5fd3126885175b9ba9df79d0c717c8a79
CarloDettori <dettoric07@gmail.com>
2024-12-11 23:03:15 +0100

    added CompletedTasksComponent()

--- src/components.jsx
+++ src/components.jsx
@@ -23,14 +23,25 @@ function BacklogProgressTasksComponent() {
         <ul>{BacklogProgressTasks}</ul>
     </>
 }
-const CompletedTasksComponent = () => (
-    <ul>
-        {items.filter((element) => (
-            <li key={index}>{item}</li>
-        ))}
-    </ul>
-);
+
+function CompletedTasksComponent() {
+    let CompletedTasks = [];
+    CompletedTasks = task.filter((element) => {
+        return element.state.includes("")
+    }).map((element) => {
+        return <li key={element.id}>
+            <h3>{element.title}</h3>
+            <p>Priority: {element.priority}</p>
+            <p>Estimated Time: {element.estimatedTime}</p>
+        </li>
+    })
+    const template = <>
+        <h2>Backlog or in Progress Task {CompletedTasks.lenght}</h2>
+        <ul>{CompletedTasks}</ul>
+    </>
+}
 
 
 module.exports = HeaderComponent;
 module.exports = BacklogProgressTasksComponent();
+module.exports = CompletedTasks();
