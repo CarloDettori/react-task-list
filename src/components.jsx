f27fe54474c36052e166a20744d36788a97c54be
CarloDettori <dettoric07@gmail.com>
2024-12-11 22:31:50 +0100

    made backlogProgressTasks

--- src/components.jsx
+++ src/components.jsx
@@ -7,3 +7,26 @@ const HeaderComponent = () => (
     </header>
 );
 
+function BacklogProgressTasksComponent() {
+    let BacklogProgressTasks = [];
+    BacklogProgressTasks = task.filter((element) => {
+        return element.state.includes("backlog" || "progress")
+    }).map((element) => {
+        return <li key={element.id}>
+            <h3>{element.title}</h3>
+            <p>Priority: {element.priority}</p>
+            <p>Estimated Time: {element.estimatedTime}</p>
+        </li>
+    })
+    const template = <>
+        <h2>Backlog or in Progress Task {BacklogProgressTasks.lenght}</h2>
+        <ul>BacklogProgressTasks</ul>
+    </>
+}
+const CompletedTasksComponent = () => (
+    <ul>
+        {items.filter((element) => (
+            <li key={index}>{item}</li>
+        ))}
+    </ul>
+);
